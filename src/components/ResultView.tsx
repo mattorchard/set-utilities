fcfae46b26c1e6d964422cf3f63558dc7d3d70fa
Matt Orchard <morch028@uottawa.ca>
2022-01-19 22:05:50 -0500

    Memoizes result view

--- src/components/ResultView.tsx
+++ src/components/ResultView.tsx
@@ -17,57 +17,59 @@ const formatLineNumber = ({ lineStart, lineEnd }: RunDocumentSegment) =>
     ? lineStart
     : `${lineStart}${nonBreakingHyphen}${lineEnd}`;
 
-const ResultView: React.FC<ResultViewProps> = ({
-  report,
-  includeHeadings = true,
-  includeLineNumbers = true,
-}) => (
-  <ol>
-    {report.map((reportItem) => (
-      <Box as="li" flexDirection="column" key={reportItem.id} mb={32}>
-        <h3 className="bp4-heading" hidden={!includeHeadings}>
-          Appears in {reportItem.docs.length} sources{" "}
-          <Typo muted>{reportItem.docs.map((doc) => doc.name).join(", ")}</Typo>
-        </h3>
-        <table className="result-view__table bp4-html-table bp4-interactive">
-          {includeLineNumbers && (
-            <thead>
-              <tr>
-                <th colSpan={reportItem.docs.length}>
-                  <Tooltip
-                    content={reportItem.docs.map((doc) => doc.name).join(", ")}
-                  >
-                    Line&nbsp;№
-                  </Tooltip>
-                </th>
-
-                <th>Segment</th>
-              </tr>
-            </thead>
-          )}
-          <tbody>
-            {reportItem.results.map((result, index) => (
-              <tr key={index}>
-                {includeLineNumbers &&
-                  result.map((result) => (
-                    <td key={result.id} title={result.doc.name}>
-                      {result.segments
-                        .map((segment) => formatLineNumber(segment))
+const ResultView: React.FC<ResultViewProps> = React.memo(
+  ({ report, includeHeadings = true, includeLineNumbers = true }) => (
+    <ol>
+      {report.map((reportItem) => (
+        <Box as="li" flexDirection="column" key={reportItem.id} mb={32}>
+          <h3 className="bp4-heading" hidden={!includeHeadings}>
+            Appears in {reportItem.docs.length} sources{" "}
+            <Typo muted>
+              {reportItem.docs.map((doc) => doc.name).join(", ")}
+            </Typo>
+          </h3>
+          <table className="result-view__table bp4-html-table bp4-interactive">
+            {includeLineNumbers && (
+              <thead>
+                <tr>
+                  <th colSpan={reportItem.docs.length}>
+                    <Tooltip
+                      content={reportItem.docs
+                        .map((doc) => doc.name)
                         .join(", ")}
-                    </td>
-                  ))}
-                <td className="result-view__table__segment">
-                  <Typo as="pre" monospace>
-                    {result[0].segments[0].original}
-                  </Typo>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </Box>
-    ))}
-  </ol>
+                    >
+                      Line&nbsp;№
+                    </Tooltip>
+                  </th>
+
+                  <th>Segment</th>
+                </tr>
+              </thead>
+            )}
+            <tbody>
+              {reportItem.results.map((result, index) => (
+                <tr key={index}>
+                  {includeLineNumbers &&
+                    result.map((result) => (
+                      <td key={result.id} title={result.doc.name}>
+                        {result.segments
+                          .map((segment) => formatLineNumber(segment))
+                          .join(", ")}
+                      </td>
+                    ))}
+                  <td className="result-view__table__segment">
+                    <Typo as="pre" monospace>
+                      {result[0].segments[0].original}
+                    </Typo>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Box>
+      ))}
+    </ol>
+  )
 );
 
 export default ResultView;
